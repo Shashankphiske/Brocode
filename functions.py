5f62d26d88a5aac51dc11f3023a4b733f053cdd1
Shashank Phiske <111897076+Shashankphiske@users.noreply.github.com>
2022-12-04 03:05:31 +0530

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,13 @@
+# Functions = A block of reusable code
+#             use () after a function to activate it or Invoke it or call it
+
+# any data you send to function are called as arguments
+# you need matching parameters to accept those arguments
+# Return = return is a statement that is used to end a function and send the result back to caller
+
+a = int(input("Enter your age :"))
+def Age(a):
+  print(f"Your age is {a}")
+Age(a)  
+  
+  
