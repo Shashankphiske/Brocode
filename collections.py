842d2484874655ae79cdfa7ebfe1921c67c6de80
Shashank Phiske <111897076+Shashankphiske@users.noreply.github.com>
2022-12-04 02:46:15 +0530

    Update collections.py

--- collections.py
+++ collections.py
@@ -5,4 +5,10 @@
 
 fruits = ["apple","orange","mango"]
 print("apple" in fruits)
+# same for others
 # this will return a boolean
+# if you want to insert a value at certain index
+fruits.insert(0,"pineapple")
+# to sort a list any order use fruits.sort()
+fruits.count("apple")
+# returns the number of times apple has occured in the list
