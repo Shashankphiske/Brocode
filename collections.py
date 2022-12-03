b54a870c2ee60dd62b1c0bfd349e684ecb7d092d
Shashank Phiske <111897076+Shashankphiske@users.noreply.github.com>
2022-12-04 02:41:02 +0530

    Create collections.py

---
+++ collections.py
@@ -0,0 +1,8 @@
+# Collections = single variable used to store multiple variables
+# List = [] ordered and changeable ,Duplicates OK
+# Tuple = () ordered and unchangeable,Duplicates Ok faster
+# Sets = {} unordered and immutable,No duplicates
+
+fruits = ["apple","orange","mango"]
+print("apple" in fruits)
+# this will return a boolean
