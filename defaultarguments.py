0a0192902be17ffa283d7c955488d278264c375e
Shashank Phiske <111897076+Shashankphiske@users.noreply.github.com>
2022-12-04 10:33:43 +0530

    Update defaultarguments.py

--- defaultarguments.py
+++ defaultarguments.py
@@ -1,1 +1,9 @@
+# Default arguments = a default value of a certain parameters 
+#                     default is used when that argument is omitted
+#                     make your function more flexible ,reduction of arguments 
 
+def add(a=2,b=2):
+  c = a+b
+  print(c)
+add()  
+# we can add our own arguments but defaults are now a = 2 and b = 2  
