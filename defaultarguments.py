8be7c8b93d04055affe570aeeb6eb7e5148bc9bf
Shashank Phiske <111897076+Shashankphiske@users.noreply.github.com>
2022-12-04 10:36:23 +0530

    Update defaultarguments.py

--- defaultarguments.py
+++ defaultarguments.py
@@ -7,3 +7,5 @@ def add(a=2,b=2):
   print(c)
 add()  
 # we can add our own arguments but defaults are now a = 2 and b = 2  
+# in keyword arguments there is a function called sep
+# it separates given arguments by a anything
