ca50ed15deb05c91335f5f41b019a47885e9a6cc
Shashank Phiske <111897076+Shashankphiske@users.noreply.github.com>
2022-12-05 23:52:24 +0530

    Update OOPs.py

--- OOPs.py
+++ OOPs.py
@@ -1,1 +1,20 @@
+# In oop there are classes who have attributes which means what they have / is
+# and they have methods which are actions that a class can do
 
+class Car:
+  def __init__(self,make,model,year,color):
+    
+    make = None
+    model = None
+    year = None 
+    color = None
+  
+  # we will make methods now
+  def drive(self):
+    
+    print("this car is driving)
+  def stop(self):
+    print("this car is stopped")      
+      
+          
+  
